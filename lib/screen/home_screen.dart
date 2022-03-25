dcc9abbd78300b334a3d1c8d69e2c2973d1b3687
yechoi42 <yechoi@student.42seoul.kr>
2022-03-25 23:05:05 +0900

    feat: add CarouselSlider at Home

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/model/model_movie.dart';
+import 'package:my_app/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -22,7 +23,12 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(children: <Widget>[
+      Stack(children: <Widget>[
+        CarouselImage(movies: movies),
+        TopBar(),
+      ])
+    ]);
   }
 }
 
---
+++ lib/widget/carousel_slider.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:my_app/model/model_movie.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie>? movies;
+
+  CarouselImage({this.movies});
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  List<Movie>? movies;
+  List<Widget>? images;
+  List<String>? keywords;
+  List<bool>? likes;
+
+  int _currentPage = 0;
+  String? _currentKeyword;
+
+  @override
+  void initState() {
+    super.initState();
+    movies = widget.movies;
+    images = movies?.map((m) => Image.asset('./images/' + m.poster)).toList();
+    keywords = movies?.map((m) => m.keyword).toList();
+    likes = movies?.map((m) => m.like).toList();
+    _currentKeyword = keywords![0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(children: <Widget>[
+      Container(
+        padding: EdgeInsets.all(20),
+      ),
+      CarouselSlider(
+        items: images,
+        options: CarouselOptions(onPageChanged: (index, reason) {
+          setState(() {
+            _currentPage = index;
+            _currentKeyword = keywords![_currentPage];
+          });
+        }),
+      ),
+      Container(
+        child: Text(_currentKeyword!),
+      )
+    ]));
+  }
+}
