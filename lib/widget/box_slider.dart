44cbb762b0ea24be1780232c2ddb9e5d39229b2b
yechoi42 <yechoi@student.42seoul.kr>
2022-04-05 23:04:51 +0900

    feat: add box_slider, circle_slider onTap action

--- lib/widget/box_slider.dart
+++ lib/widget/box_slider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/model/model_movie.dart';
+import 'package:my_app/screen/detail_screen.dart';
 
 class BoxSlider extends StatelessWidget {
   final List<Movie>? movies;
@@ -17,16 +18,22 @@ class BoxSlider extends StatelessWidget {
                   height: 120,
                   child: ListView(
                       scrollDirection: Axis.horizontal,
-                      children: makeBoxImages(movies!)))
+                      children: makeBoxImages(context, movies!)))
             ]));
   }
 }
 
-List<Widget> makeBoxImages(List<Movie> movies) {
+List<Widget> makeBoxImages(BuildContext context, List<Movie> movies) {
   List<Widget> results = [];
   for (var i = 0; i < movies.length; i++) {
     results.add(InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute<Null>(
+              fullscreenDialog: true,
+              builder: (BuildContext context) {
+                return DetailScreen(movie: movies[i]);
+              }));
+        },
         child: Container(
             padding: EdgeInsets.only(right: 10),
             child: Align(
